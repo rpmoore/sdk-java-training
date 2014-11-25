10b9bdd52825bd7d6d38edeb158f193e825ed004
rpmoore <ryan.moore@greyrocksoft.com>
2014-11-25 16:07:51 -0700

    Updated example

--- src/main/java/com/spectralogic/sdktraining/PutDirectory.java
+++ src/main/java/com/spectralogic/sdktraining/PutDirectory.java
@@ -17,24 +17,36 @@ import com.spectralogic.ds3client.serializer.XmlProcessingException;
 public class PutDirectory {
 
     public static void main(final String[] args) throws SignatureException, IOException, XmlProcessingException {
-        final Ds3Client client = Ds3ClientBuilder.create("ds3_endpoint", new Credentials("access_key", "secret_key"))
+        final Ds3Client client = Ds3ClientBuilder.create("192.168.56.101:8080", new Credentials("c3BlY3RyYQ==", "m7AsqEnq"))
         		.withHttps(false) // Turn off SSL
         		.build();
         
         final Ds3ClientHelpers helpers = Ds3ClientHelpers.wrap(client);
         
-        final String bucketName = "testBucket";
-        
-        // Need a call to make sure the bucket we are going to put data to exists
+        final String bucketName = "testBucket";        
         
         final Path pathToArchive = Paths.get("src/main/resources/books");
+
+        archive(helpers, bucketName, pathToArchive);
+        archive(helpers, "differentBucket", Paths.get("something/path"));
+        
+        System.out.println("Finished writing objects to Black Pearl");
+    }
+    
+    private static void archive(final Ds3ClientHelpers helper, final String bucketName, final Path path) throws SignatureException, IOException, XmlProcessingException {
+        // Need a call to make sure the bucket we are going to put data to exists
+        helper.ensureBucketExists(bucketName);
         
         // Get the list of all the files in a directory
+        
+        final Iterable<Ds3Object> objects = helper.listObjectsForDirectory(path);
         
         System.out.println("Starting bulk put job");
         
         // Start the bulk job and put all the objects to black pearl
         
-        System.out.println("Finished writing objects to Black Pearl");
+        Job job = helper.startWriteJob(bucketName, objects);
+        
+        job.transfer(new FileObjectPutter(path));    	
     }
 }
